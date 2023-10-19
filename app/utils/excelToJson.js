b7f443afd295c91e0bb688f834912376e4dc6074
Ido <ido.deu@gmail.com>
2023-10-19 19:26:57 +0300

    fix undefined bug

--- app/utils/excelToJson.js
+++ app/utils/excelToJson.js
@@ -1,76 +1,100 @@
-const ExcelJS = require('exceljs');
+const ExcelJS = require("exceljs");
 
-const colOfLocations=50;
+let colOfLocations;
 const rowOfUnits = 4;
 
-
 const getUnitsName = async (worksheet) => {
-    const unitsRow = worksheet.getRow(rowOfUnits)
-    //Remove Duplicate 
-    const units = unitsRow.values.filter((value, index, self) => {
-        return self.indexOf(value) === index;
-    });
-    return units;
-}
+  const unitsRow = worksheet.getRow(rowOfUnits);
+  //Remove Duplicate
+  const units = unitsRow.values.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  });
+  return units;
+};
 
 const getLocations = async (worksheet) => {
-    const locationsCol = worksheet.getColumn(colOfLocations).values;
-    const locations = locationsCol.slice(locationsCol.indexOf('אוכלוסיה')+2, locationsCol.indexOf('כ\"סה'))
-    return locations;
-}
+  const locationsCol = worksheet.getColumn(colOfLocations).values;
+  const locations = locationsCol.slice(
+    locationsCol.indexOf("אוכלוסיה") + 2,
+    locationsCol.indexOf('כ"סה')
+  );
+  return locations;
+};
 
-const getCell = async (row,col, worksheet) => {
-    return worksheet.getRow(row).values[col];
-}
+const getCell = async (row, col, worksheet) => {
+  return worksheet.getRow(row).values[col];
+};
 
- 
 function createObjectWithoutData(unitsArray, locationsArray) {
-    const result = {};
+  const result = {};
 
-    locationsArray.forEach(location => {
-        result[location] = {
-            sadir: unitsArray.reduce((acc, unit) => {
-                acc[unit] = null;
-                return acc;
-            }, {}),
-            reserve: unitsArray.reduce((acc, unit) => {
-                acc[unit] = null;
-                return acc;
-            }, {})
-        };
-    });
+  locationsArray.forEach((location) => {
+    result[location] = {
+      sadir: unitsArray.reduce((acc, unit) => {
+        acc[unit] = null;
+        return acc;
+      }, {}),
+      reserve: unitsArray.reduce((acc, unit) => {
+        acc[unit] = null;
+        return acc;
+      }, {}),
+    };
+  });
 
-    return result;
+  return result;
 }
-
-
 
 const getIndexOfUnit = (unitName, worksheet) => {
-    return worksheet.getRow(rowOfUnits).values.indexOf(unitName);
-}
+  return worksheet.getRow(rowOfUnits).values.indexOf(unitName);
+};
 
 const getIndexOfLocation = (locationName, worksheet) => {
-    return worksheet.getColumn(colOfLocations).values.indexOf(locationName);
+  return worksheet.getColumn(colOfLocations).values.indexOf(locationName);
+};
+
+function removeUndefined(obj) {
+  for (let key in obj) {
+    if (obj[key] === undefined) {
+      delete obj[key];
+    } else if (typeof obj[key] === "object") {
+      removeUndefined(obj[key]);
+    }
+  }
+  return obj;
 }
 
 export const getExcelinJson = async (worksheet) => {
-    const data = createObjectWithoutData(await getUnitsName(worksheet), await getLocations(worksheet));
+  colOfLocations = worksheet.getRow(6).values.indexOf("אוכלוסיה");
 
-    Object.keys(data).forEach(locationName => {
-        const locationRow = getIndexOfLocation(locationName, worksheet)
-        
-        Object.keys(data[locationName]["sadir"]).forEach(async (unitName) => {
-            const unitIndex =  getIndexOfUnit(unitName, worksheet)
-            const sadirCol = unitIndex+1;
-            data[locationName]['sadir'][unitName] = await getCell(locationRow,sadirCol, worksheet);
-        })
-        Object.keys(data[locationName]["reserve"]).forEach(async (unitName) => {
-            const unitIndex =  getIndexOfUnit(unitName, worksheet)
-            const reserveCol = unitIndex;
-            data[locationName]['reserve'][unitName] = await getCell(locationRow,reserveCol, worksheet );
-        })
-          
-    })
+  const data = createObjectWithoutData(
+    await getUnitsName(worksheet),
+    await getLocations(worksheet)
+  );
 
-    return data
-}
+  Object.keys(data).forEach((locationName) => {
+    const locationRow = getIndexOfLocation(locationName, worksheet);
+
+    Object.keys(data[locationName]["sadir"]).forEach(async (unitName) => {
+      const unitIndex = getIndexOfUnit(unitName, worksheet);
+      const sadirCol = unitIndex + 1;
+
+      const cellContent = await getCell(locationRow, sadirCol, worksheet);
+      if (cellContent) {
+        data[locationName]["sadir"][unitName] = cellContent;
+      } else {
+        delete data[locationName]["sadir"][unitName];
+      }
+    });
+    Object.keys(data[locationName]["reserve"]).forEach(async (unitName) => {
+      const unitIndex = getIndexOfUnit(unitName, worksheet);
+      const reserveCol = unitIndex;
+      const cellContent = await getCell(locationRow, reserveCol, worksheet);
+      if (cellContent) {
+        data[locationName]["reserve"][unitName] = cellContent;
+      } else {
+        delete data[locationName]["reserve"][unitName];
+      }
+    });
+  });
+  return removeUndefined(data);
+};
